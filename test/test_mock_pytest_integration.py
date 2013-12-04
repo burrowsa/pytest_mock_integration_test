e132c4081fd18339f4f2f1e3e034b70823ad1a9f
Andrew Burrows <burrowsa@gmail.com>
2013-12-04 00:46:53 +0000

    Enable tests for other python versions

--- test/test_mock_pytest_integration.py
+++ test/test_mock_pytest_integration.py
@@ -1,8 +1,22 @@
 import subprocess
 import os
 
+
+def check_output(*args, **kwargs):
+    if hasattr(subprocess, "check_output"):
+        return subprocess.check_output(*args, **kwargs)
+
+    process = subprocess.Popen(*args, stdout=subprocess.PIPE, **kwargs)
+    output, unused_err = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        raise subprocess.CalledProcessError(retcode, args[0])
+    return output
+
+
 def test_test_names_are_not_corrupted_by_patching():
-    result = subprocess.check_output([os.environ["PYTEST_EXECUTABLE"], "-v", os.path.join(os.path.dirname(__file__), "test_example.py")]).split(os.linesep)
-    assert result[4].endswith("test_one PASSED")
-    assert result[5].endswith("test_two PASSED")
-    assert result[6].endswith("test_three PASSED")
+    result = check_output(["py.test", "-v", "test/test_example.py"], shell=True)
+    lines = result.split(os.linesep)
+    assert lines[4].endswith("test_one PASSED")
+    assert lines[5].endswith("test_two PASSED")
+    assert lines[6].endswith("test_three PASSED")
