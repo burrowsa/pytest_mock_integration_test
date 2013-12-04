c8108bb18a60a2d4c80a8e75f30ec5819a317859
Andrew Burrows <burrowsa@gmail.com>
2013-12-04 00:14:04 +0000

    more debugging travis

--- test/test_mock_pytest_integration.py
+++ test/test_mock_pytest_integration.py
@@ -3,12 +3,6 @@ import os
 
 def test_test_names_are_not_corrupted_by_patching():
     result = subprocess.check_output([os.environ["PYTEST_EXECUTABLE"], "-v", os.path.join(os.path.dirname(__file__), "test_example.py")]).split(os.linesep)
-    
-    print "**************************************"
-    for line in result:
-        print line
-    print "**************************************"
-    
     assert result[4].endswith("test_one PASSED")
     assert result[5].endswith("test_two PASSED")
     assert result[6].endswith("test_three PASSED")
